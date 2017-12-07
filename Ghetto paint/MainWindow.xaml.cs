d86d920a37bea4aa200672e8bd842c8766190cb4
Bo Loomans <bo.loomans204947@student.leeuwenborgh.nl>
2017-12-07 09:22:00 +0100

    da

--- Ghetto paint/MainWindow.xaml.cs
+++ Ghetto paint/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Ghetto_paint
 {
@@ -20,20 +21,34 @@ namespace Ghetto_paint
     /// </summary>
     public partial class MainWindow : Window
     {
+        //public Thread thread;
         public Point pointToWindow;
         public Brush AppColor;
         public int SelectedObject;
         public string SelectedColor = null;
+        public bool MouseDraw;
 
         public MainWindow()
         {
             InitializeComponent();
             AppColor = Brushes.Blue;
+            Thread thread = new Thread(EraserThread);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
         }
 
-        private void btnCircle_Click(object sender, RoutedEventArgs e)
+        private void slWidth_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            SelectedObject = 1;
+            lblWidth.Content = Convert.ToInt16(slWidth.Value);
+        }
+
+        private void slHeight_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblHeight.Content = Convert.ToInt16(slHeight.Value);
+        }
+        private void slThickness_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblThickness.Content = Convert.ToInt16(slThickness.Value);
         }
 
         private void Ellipse()
@@ -46,21 +61,6 @@ namespace Ghetto_paint
             Dock.Children.Add(ellipse);
         }
 
-        private void slWidth_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            lblWidth.Content = Convert.ToInt16(slWidth.Value);
-        }
-
-        private void slHeight_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            lblHeight.Content = Convert.ToInt16(slHeight.Value);
-        }
-
-        private void btnRect_Click(object sender, RoutedEventArgs e)
-        {
-            SelectedObject = 2;
-        }
-
         private void Rectangle()
         {
             Rectangle rectangle = new Rectangle();
@@ -69,6 +69,53 @@ namespace Ghetto_paint
             rectangle.Stroke = AppColor;
             rectangle.Margin = new Thickness(pointToWindow.X, pointToWindow.Y, 0, 0);
             Dock.Children.Add(rectangle);
+        }
+
+        private void Pencil()
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = slThickness.Value;
+            ellipse.Height = slThickness.Value;
+            ellipse.Fill = AppColor;
+            ellipse.Stroke = AppColor;
+            ellipse.Margin = new Thickness(pointToWindow.X - slThickness.Value/2, pointToWindow.Y - slThickness.Value/2, 0, 0);
+            Dock.Children.Add(ellipse);
+        }
+
+        private void EraserThread()
+        {
+            while (true)
+            {
+
+                Thread.Sleep(10);
+                
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    if (SelectedObject == 3 || SelectedObject == 4)
+                    {
+                        Aids();
+                        MouseUI.Children.Clear();                       
+                        Ellipse ellipse = new Ellipse();
+                        ellipse.Width = slThickness.Value;
+                        ellipse.Height = slThickness.Value;
+                        ellipse.Stroke = Brushes.DimGray;
+                        ellipse.Margin = new Thickness(pointToWindow.X - slThickness.Value / 2, pointToWindow.Y - slThickness.Value / 2, 0, 0);
+                        MouseUI.Children.Add(ellipse);
+                    }
+                }));
+            }
+        }
+
+        private void Eraser()
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = slThickness.Value;
+            ellipse.Height = slThickness.Value;
+            ellipse.Fill = Brushes.White;
+            ellipse.Stroke = Brushes.White;
+            ellipse.Margin = new Thickness(pointToWindow.X - slThickness.Value / 2, pointToWindow.Y - slThickness.Value / 2, 0, 0);
+            Dock.Children.Add(ellipse);
         }
 
         private void cmbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -100,27 +147,55 @@ namespace Ghetto_paint
 
         public Point Aids()
         {
-                Point pointToScreen = Mouse.GetPosition(this);
-                pointToWindow = pointToScreen;
-                return pointToScreen;
+            Point pointToScreen = Mouse.GetPosition(this);
+            pointToWindow = pointToScreen;
+            return pointToScreen;
         }
 
         private void MousDown(object sender, MouseButtonEventArgs e)
         {
             Aids();
+            MouseDraw = true;
             if (SelectedObject == 1)
             {
                 Ellipse();
-            } else if (SelectedObject == 2)
-            {
-                btnRect_Click(sender, e);
             }
-
+            else if (SelectedObject == 2)
+            {
+                Rectangle();
+            }
         }
-        private void btnLine_Click(object sender, MouseButtonEventArgs e)
+
+        private void btnCircle_Click(object sender, RoutedEventArgs e)
         {
-            btnLine.IsEnabled = false;
-            btnLine.Fill = Brushes.Aqua;
+            SelectedObject = 1;
+        }
+        private void btnRect_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedObject = 2;
+        }
+        private void btnPencil_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedObject = 3;
+        }
+        private void btnEraser_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedObject = 4;
+        }
+        private void MainGrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            Aids();
+            if (e.LeftButton == MouseButtonState.Pressed && SelectedObject == 3)
+            {
+                Pencil();
+            } else if (e.LeftButton == MouseButtonState.Pressed && SelectedObject == 4)
+            {
+                Eraser();
+            }
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            Dock.Children.Clear();
         }
     }
 }
